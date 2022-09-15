6c73f20b8fdb4b39de5ad315b1abed8c04d71bf3
Ivan Okhmatovsky <ivan.okhmatovsky@frtk.ru>
2022-09-15 01:15:36 -0400

    Added perfect magnetic conductor BC code

--- 1D_ExHy_mode_PMC_BC.py
+++ 1D_ExHy_mode_PMC_BC.py
@@ -29,8 +29,8 @@ if __name__=="__main__":
     omega = 2 * np.pi * f # angular frequency [Hz]
     k = omega / sp.constants.c # wave number [1/m]
 
-    Ey = np.zeros(Nz+1) # E(0) .... E(Nz) # for PEC BC
-    Hx = np.zeros(Nz) # H(0) .... H(Nz) # for PMC BC
+    Ey = np.zeros(Nz) # E(0) .... E(Nz) # for PEC BC
+    Hx = np.zeros(Nz+1) # H(0) .... H(Nz) # for PMC BC
     Mx = np.zeros(Nt) # magnetic current density -- variation in time at the source point
     Jy = np.zeros(Nt) # electric current density -- variation in time at the source point
     T = np.arange(0, Nt*dt, dt)
@@ -49,26 +49,26 @@ if __name__=="__main__":
 
 
 
-    # PEC
+    # PMC
     #
-    # E_H_E_H_E_H_E_H_E_H_E_H_E_H_E_H_E
+    # H_E_H_E_H_E_H_E_H_E_H_E_H_E_H_E_H
     # |___|___|___|___|___|___|___|___|
     # 0___|___|___|___|___|___|___|___0
     #
-    # for PEC BC we set E(0)=0 and E(Nz)=0, values right at the  boundary and not out of it
-    # E array has Nz+1 elements
-    # H array has Nz elements
+    # for PMC BC we set H(0)=0 and H(Nz)=0, values right at the  boundary and not out of it
+    # H array has Nz+1 elements
+    # E array has Nz elements
 
 
     # The version with PEC boundary condition -- electric field is reflected with the same phase
     # Warning: magnetic field is normalized in these update equations, thus E and H have the same magnitudes
-    def time_step_PEC(n):
+    def time_step_PMC(n):
         # update magnetic field from electric
-        Hx[0:Nz] += mHx[0:Nz] * (Ey[1:Nz+1] - Ey[0:Nz]) / dz - mHx[0:Nz] * Mx[n]
+        Hx[1:Nz] += mHx[1:Nz] * (Ey[1:Nz] - Ey[0:Nz-1]) / dz - mHx[1:Nz] * Mx[n]
+        Hx[0] = 0
+        Hx[Nz] = 0
         # update electric field from magnetic
-        Ey[1:Nz] += mEy[1:Nz] * (Hx[1:Nz] - Hx[0:Nz - 1]) / dz  # eta_0 is free space impedance - from the normalization of magnetic field
-        Ey[0] = 0
-        Ey[Nz] = 0
+        Ey[0:Nz] += mEy[0:Nz] * (Hx[1:Nz+1] - Hx[0:Nz]) / dz  # eta_0 is free space impedance - from the normalization of magnetic field
         Ey[int(Nz/3)] -= mEy[int(Nz/3)] * eta_0 * Jy[n]
 
         return Ey, Hx
@@ -79,8 +79,8 @@ if __name__=="__main__":
     fig, ax = plt.subplots()
     ax.plot([int(Nz/3)*dz, int(Nz/3)*dz], [-50, 50], color='green')
     plt.text(int(Nz/3)*dz+1,-48, 'source \n location', fontsize=8)
-    line0, = ax.plot(np.arange(0, (Nz+1)*dz, dz), Ey, c='b', label=r'$E_y,~[\frac{V}{m}]$')
-    line1, = ax.plot(np.arange(0, (Nz)*dz, dz)+dz/2, Hx, c='r', label=r'$\tilde{H}_x = \eta_0 H_x = E_y,~[\frac{V}{m}]$')
+    line0, = ax.plot(np.arange(0, (Nz)*dz, dz)+dz/2, Ey, c='b', label=r'$E_y,~[\frac{V}{m}]$')
+    line1, = ax.plot(np.arange(0, (Nz+1)*dz, dz), Hx, c='r', label=r'$\tilde{H}_x = \eta_0 H_x = E_y,~[\frac{V}{m}]$')
     plt.xlabel('distance, m')
     plt.ylabel('amplitude of Ey, V/m')
 
@@ -97,7 +97,7 @@ if __name__=="__main__":
 
 
     def animate(n):
-        Ey, Hx= time_step_PEC(n)
+        Ey, Hx= time_step_PMC(n)
         line0.set_ydata(Ey)  # update the data
         line1.set_ydata(Hx)  # update the data
         ax.set_title("time={:.2f} nsec".format(n*dt*1e+9))
@@ -106,7 +106,7 @@ if __name__=="__main__":
 
     ani = animation.FuncAnimation(fig=fig, func=animate, frames=Nt, fargs=(), init_func=anim_init, blit=True)
 
-    f = r"./1D_EyHx_mode_PEC_BC.mp4"
+    f = r"./1D_EyHx_mode_PMC_BC.mp4"
     writervideo = animation.FFMpegWriter(fps=20)
     ani.save(f, writer=writervideo)
 
